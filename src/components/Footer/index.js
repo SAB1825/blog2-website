fc524ca68211b570aab03cfa2a4f00fc543f8251
SABARI <sabarisekar777@gmail.com>
2023-09-29 21:40:27 +0530

    blog2-website

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,5 @@
-"use client";
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { DribbbleIcon, GithubIcon, LinkedinIcon, TwitterIcon } from "../Icons";
 import Link from "next/link";
@@ -11,35 +11,50 @@ const Footer = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const onSubmit = (data) => {
+    console.log(data);
+    setIsSubscribed(true); 
+  };
   console.log(errors);
 
   return (
     <footer className="mt-16 rounded-2xl bg-dark dark:bg-accentDark/90 m-2 sm:m-10 flex flex-col items-center text-light dark:text-dark">
-      <h3 className="mt-16 font-medium dark:font-bold text-center capitalize text-2xl sm:text-3xl lg:text-4xl px-4">
-        Interesting Stories | Updates | Guides
-      </h3>
-      <p className="mt-5 px-4 text-center w-full sm:w-3/5 font-light dark:font-medium text-sm sm:text-base">
-        Subscribe to learn about new technology and updates. Join over 5000+
-        members community to stay up to date with latest news.
-      </p>
+      {isSubscribed ? (
+        <div className="mt-6">
+          <p className="text-light dark:text-dark text-lg font-medium">
+            Thank you for signing up!
+          </p>
+        </div>
+      ) : (
+        <>
+          <h3 className="mt-16 font-medium dark:font-bold text-center capitalize text-2xl sm:text-3xl lg:text-4xl px-4">
+            Interesting Stories | Updates | Guides
+          </h3>
+          <p className="mt-5 px-4 text-center w-full sm:w-3/5 font-light dark:font-medium text-sm sm:text-base">
+            Subscribe to learn about new technology and updates. Join over 5000+
+            members community to stay up to date with latest news.
+          </p>
 
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="mt-6 w-fit sm:min-w-[384px] flex items-stretch bg-light dark:bg-dark p-1 sm:p-2 rounded mx04"
-      >
-        <input
-          type="email"
-          placeholder="Enter your email"
-          {...register("email", { required: true, maxLength: 80 })}
-          className="w-full bg-transparent pl-2 sm:pl-0 text-dark focus:border-dark focus:ring-0 border-0 border-b mr-2 pb-1"
-        />
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="mt-6 w-fit sm:min-w-[384px] flex items-stretch bg-light dark:bg-dark p-1 sm:p-2 rounded mx04"
+          >
+            <input
+              type="email"
+              placeholder="Enter your email"
+              {...register("email", { required: true, maxLength: 80 })}
+              className="w-full bg-transparent pl-2 sm:pl-0 text-dark focus:border-dark focus:ring-0 border-0 border-b mr-2 pb-1"
+            />
 
-        <input
-          type="submit"
-          className="bg-dark text-light dark:text-dark dark:bg-light cursor-pointer font-medium rounded px-3 sm:px-5 py-1"
-        />
-      </form>
+            <input
+              type="submit"
+              className="bg-dark text-light dark:text-dark dark:bg-light cursor-pointer font-medium rounded px-3 sm:px-5 py-1"
+            />
+          </form>
+        </>
+      )}
+
       <div className="flex items-center mt-8">
         <a
           href={siteMetadata.linkedin}
